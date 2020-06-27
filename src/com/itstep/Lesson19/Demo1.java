d1aa1b6c8c1c83bbff9de7ab1521012d14e048e9
TatianaS8 <cont.08.2012@gmail.com>
2020-06-27 15:38:00 +0300

    Added main method to GitDemo1

--- src/com/itstep/Lesson19/Demo1.java
+++ src/com/itstep/Lesson19/Demo1.java
@@ -3,6 +3,7 @@ package com.itstep.Lesson19;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("My changes");
+        System.out.println("New future");
 
     }
 }
